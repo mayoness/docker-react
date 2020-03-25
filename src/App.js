45bbec6f511a714ad18e24ac9e1f155cec4c55cb
Mariusz Borys <mariusz.borys@infor.com>
2020-03-25 08:38:16 +0100

    code change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          naprawiło sie
         </p>
         <a
           className="App-link"
